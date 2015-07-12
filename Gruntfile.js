c4c515d0dfb6946eca71bb078abfd3de10296ad8
Niek de Greef <niek@beyerdegreef.com>
2015-07-12 21:40:05 +0200

    Added Susy to project (gem installed globally)

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
 		    options: {
 				style: 'expanded',
 				sourcemap: 'auto',
+				require: 'susy'
 		    },
 		    files: {
 		    	'style.css': 'sass/style.scss'
